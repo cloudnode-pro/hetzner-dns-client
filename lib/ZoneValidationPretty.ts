3e0dcee9b8ca8f835bb7bb5efcf181874c17d7e1
williamd5 <willduncannn@gmail.com>
2023-01-20 20:09:24 +0200

    remove redundant constructor

--- lib/ZoneValidationPretty.ts
+++ lib/ZoneValidationPretty.ts
@@ -1,6 +1,5 @@
 import ZoneValidation from "./models/ZoneValidation";
 import ClientObject from "./ClientObject.js";
-import HetznerDnsClient from "./HetznerDnsClient.js";
 import DnsRecord from "./DnsRecord.js";
 
 /**
@@ -21,13 +20,4 @@ export default class ZoneValidationPretty extends ClientObject<ZoneValidation> {
      * @type {DnsRecord[]}
      */
     public readonly validRecords = this._data.valid_records.map((record) => new DnsRecord(this.client, record));
-
-    /**
-     * Create a new zone validation object
-     * @param {HetznerDnsClient} client - API client instance
-     * @param {ZoneValidation} data - Raw zone validation data
-     */
-    public constructor(client: HetznerDnsClient, data: ZoneValidation) {
-        super(client, data);
-    }
 }
